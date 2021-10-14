79f6eda2a2b6a97944928851cdf6493e9c8464bc
DenysSidorov <000scorpions000@gmail.com>
2021-10-14 22:15:13 +0300

    Added Spinner1 component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import {Switch, Route, Link} from 'react-router-dom';
 import Card1 from './examples/card-1/card1';
+import Spinner1 from './examples/spinner-1/spinner1';
 
 function App() {
   return (
     <div className="App">
       <nav>
             <Link to="/">Home  |  </Link>
+            <Link to="/spinner-1">  Spinner  |</Link>
             <Link to="/card-1">  Card  |</Link>
       </nav>
       <Switch>
-        <Route path="/card-1">
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/card-1">
           <Card1/>
         </Route>
-        <Route path="/">
-          <Home/>
+        <Route exact path="/spinner-1">
+          <Spinner1/>
         </Route>
       </Switch>
     </div>
---
+++ src/examples/spinner-1/spinner1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.css';
+
+const Spinner1 = () => {
+  return (
+    <div className={style.parent}>
+      <div className={style.spinner}/>
+    </div>
+  );
+};
+
+export default Spinner1;
