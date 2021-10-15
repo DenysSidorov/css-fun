2e15ee7bd93441724addbd505216b385e53e5b73
DenysSidorov <000scorpions000@gmail.com>
2021-10-15 23:34:17 +0300

    Improved Spinner1 component, v2

--- src/examples/spinner-1/spinner1.js
+++ src/examples/spinner-1/spinner1.js
@@ -4,7 +4,12 @@ import style from './index.module.css';
 const Spinner1 = () => {
   return (
     <div className={style.parent}>
-      <div className={style.spinner}/>
+      <div className={style.spinner}>
+        <div className={style.content}>
+          Loading
+        </div>
+        <div className={style.border}/>
+      </div>
     </div>
   );
 };
